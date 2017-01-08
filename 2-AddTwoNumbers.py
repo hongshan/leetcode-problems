1fab59da23b79d5473ec2db6e154010ff7a096bc
Hong Shan <hongshan@machine002.local>
2017-01-08 15:25:56 +0800

    resolve two problems

---
+++ 2-AddTwoNumbers.py
@@ -0,0 +1,40 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        head = ListNode(0)
+        current = head
+        def nextval(l):
+            return l.next.val if (l and l.next) else 0
+        def nextnode(l):
+            return l.next if l else None
+        v1 = l1.val
+        v2 = l2.val
+        leading = 0
+        while l1 or l2:
+            print(v1, v2)
+            sumval = v1 + v2 + leading
+            leading = sumval / 10
+            sumval = sumval % 10
+            temp = ListNode(sumval)
+            current.next = temp
+            current = current.next
+            v1 = nextval(l1)
+            v2 = nextval(l2)
+            l1 = nextnode(l1)
+            l2 = nextnode(l2)
+        if leading:
+            temp = ListNode(leading)
+            current.next = temp
+
+        return head.next
+        
