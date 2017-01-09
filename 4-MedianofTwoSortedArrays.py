09d28e190d4d5813d0fcb7c0d0d50d4240fd782c
hongshan <hongshan.mail@gmail.com>
2017-01-09 21:48:08 +0800

    remove chinese comment

--- 4-MedianofTwoSortedArrays.py
+++ 4-MedianofTwoSortedArrays.py
@@ -72,11 +72,5 @@ class Solution(object):
             
 
 # test
-# 找 nums1 numb2 中 的第m+n/2大的那个数
-# 对两个数组进行折半查找，例如nums1[1~x／2]都小于nums2[1~x/2]
-# 则说明nums1[1~x/2]都是nums1与nums2合并后小于第x个数字
-# 则在接下来的遍历中排出nums1[1~x/2]
-# 也就是我们的查找范围缩小了 x/2 
-# x = (m+n)/2) / 2
 test = Solution()
 print(test.findMedianSortedArrays([1,3,4,7,33,100,444,3333,33333], [1,1,1,1,1]))
