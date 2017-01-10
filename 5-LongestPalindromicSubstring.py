3e7d8e5846988a487459acc9e7de8d98f061bf2a
hongshan <hongshan.mail@gmail.com>
2017-01-10 18:19:39 +0800

    accept

--- 5-LongestPalindromicSubstring.py
+++ 5-LongestPalindromicSubstring.py
@@ -3,63 +3,48 @@ class Solution(object):
         """
         :type s: str
         :rtype: str
-        """
-        max_length = 0
-        longest_str = ""
-        current_str = ""
-        total_length = len(s)
-        print(total_length)
-        longest_str = s[0]
-        for i in range(total_length):
-            print("start", i)
-            prob_max_length = total_length - i if i + 1 > total_length - i else i + 1
-            current_str = s[i]
-            p1_left = i - 1
-            p1_right = i + 1
-            if prob_max_length < max_length:
-                print("nothing")
-                continue
+        """ 
+        def getLongestByPoint(point, s, isPointSymmetry):
+            longest_str = s[point]
+            if isPointSymmetry:
+                current_str = s[point]
+                left = point - 1
+                right = point + 1
+            else:
+                current_str=""
+                left = point
+                right = point + 1
+
+            total_length = len(s)
             while True:
-                print(p1_left, p1_right)
-                if p1_left < 0 or p1_right > total_length - 1: 
-                    print("no hope", p1_left, p1_right)
+                if left < 0 or right > total_length - 1: 
                     if len(current_str) > len(longest_str):
                             longest_str = current_str
                     break
                 else:
-                    if s[p1_left] == s[p1_right]:
-                        print("find same")
-                        current_str = s[p1_left] + current_str + s[p1_right]
+                    if s[left] == s[right]:
+                        current_str = s[left] + current_str + s[right]
                     else:
                         if len(current_str) > len(longest_str):
                             longest_str = current_str
-                            print("find long", longest_str)
                         break
-                p1_left -= 1
-                p1_right += 1
-
-            # p1_left = i
-            # p1_right = i + 1
-            # while True:
-            #     print(p1_left, p1_right)
-            #     if p1_left < 0 or p1_right > total_length - 1: 
-            #         print("no hope", p1_left, p1_right)
-            #         if len(current_str) > len(longest_str):
-            #                 longest_str = current_str
-            #         break
-            #     else:
-            #         if s[p1_left] == s[p1_right]:
-            #             print("find same")
-            #             current_str = s[p1_left] + current_str + s[p1_right]
-            #         else:
-            #             if len(current_str) > len(longest_str):
-            #                 longest_str = current_str
-            #                 print("find long", longest_str)
-            #             break
-            #     p1_left -= 1
-            #     p1_right += 1
-            print("next")
+                left -= 1
+                right += 1
+            return longest_str
+        total_length = len(s)
+        longest_str = s[0]
+        for i in range(total_length):
+            prob_max_length = (total_length - i) * 2 if i + 1 > total_length - i else (i + 1) * 2
+            if prob_max_length < len(longest_str):
+                continue
+            point_str = getLongestByPoint(i, s, True)
+            longest_str = point_str if len(point_str) > len(longest_str) else longest_str
+            none_point_str = getLongestByPoint(i, s, False)
+            longest_str = none_point_str if len(none_point_str) > len(longest_str) else longest_str
         return longest_str
 
 test = Solution()
-print(test.longestPalindrome("abcbghghabcddcbaff"))
+input_str = "abcabba"
+print(input_str, len(input_str))
+output = test.longestPalindrome(input_str)
+print(output, len(output))
