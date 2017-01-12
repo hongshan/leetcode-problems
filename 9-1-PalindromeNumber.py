060daeb90029b770a1e89d0c7623d0d007a54624
hongshan <hongshan.mail@gmail.com>
2017-01-13 00:44:30 +0800

    some improvement

--- 9-1-PalindromeNumber.py
+++ 9-1-PalindromeNumber.py
@@ -10,7 +10,10 @@ class Solution(object):
         base = x
         while x:
             out = out * 10 +  x % 10
+            # more slow.......
+            if out == base:
+                return True
             x = x / 10
         return out == base
 test = Solution()
-print(test.isPalindrome(12))
+print(test.isPalindrome(1221))
