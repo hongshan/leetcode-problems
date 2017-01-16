5d9eaeec0f011136cdc55e67c3da3710990cadba
hongshan <hongshan.mail@gmail.com>
2017-01-17 01:03:11 +0800

    partly

--- 10-RegularExpressionMatching.py
+++ 10-RegularExpressionMatching.py
@@ -5,26 +5,34 @@ class Solution(object):
         :type p: str
         :rtype: bool
         """
-        class StateNormal(value):
-            def __init__(self, value):
-            # functions to get next state
-                self.nexts = []
-                self.value = value
-            def accept(self, input_cha):
-                next_state = None
-                for f in self.nexts:
-                    next_state = f(input_cha)
-                    if next_state:
-                        break
-                if next_state:
-                    return True, next_state
-                else:
-                    return False
+class State():
+    def __init__(self, value):
+    # functions to get next state
+        # should be a function return a next state
+        self.nexts = []
+        self.value = value
+        self.number = 0
+    def setNexts(self, nexts):
+        self.nexts = nexts
+    def accept(self, input_cha):
+        next_state = None
+        for f in self.nexts:
+            next_state = f(input_cha)
+            if next_state:
+                break
+        else:
+            # end of state
+            return None
+        if next_state:
+            return next_state
+        else:
+            return False
 class StateController():
     """docstring for StateController"""
     def __init__(self, pattern):
         self.pattern = pattern
         self.stateToken = []
+        self.states = []
     def checkPatternValid(self):
         if self.pattern[0] == '*':
             return False
@@ -52,8 +60,103 @@ class StateController():
                 i += 1
         print(self.stateToken)
         return True
+    def getSingleAcceptedFunction(self, expect_value, next_state, pre_common_state):
+        def nextFunction(input):
+            print("============")
+            if expect_value == '.':
+                print("input", input)
+                print('current expection', expect_value)
+                if next_state:
+                    return True, self.states[next_state]
+                else:
+                    return True, None
+            else:
+                print("input", input)
+                print('current expection', expect_value)
+                if input == expect_value:
+                    if next_state:
+                        return True, self.states[next_state]
+                    else:
+                        return True, None
+                else:
+                    if pre_common_state:
+                        return True, self.states[pre_common_state]
+                    else:
+                        return False
+        return nextFunction
+    def getRepetAcceptedFunction(self, expect_value, next_expect_value, \
+        current_state, next_state, pre_common_state):
+        def nextFunction(input):
+            print("repet input", input, expect_value)
+            if expect_value == '.*':
+                if next_expect_value and input == next_expect_value[0]:
+                    return True, self.states[next_state]
+                else:
+                    return True, self.states[current_state]
+            else:
+                if next_expect_value and input == next_expect_value[0]:
+                    return True, self.states[next_state]
+                elif input == expect_value[0]:
+                    return True, self.states[current_state]
+                else:
+                    if pre_common_state:
+                        return True, self.states[pre_common_state]
+                    return False
+        return nextFunction
+
     def genstates(self):
-        pass
-        # for t in self.stateToken:
-test = StateController("a*b*.*aaaaa......")
-test.preProcessPattern()
+        self.preProcessPattern()
+        for i in range(len(self.stateToken)):
+
+            # self.states.append([])
+            # current_state = self.states[len(self.states) - 1]
+            next_state = None
+            pre_common_state = None
+            next_expect_value = None
+            if len(self.stateToken[i]) == 1:
+                if i + 1 < len(self.stateToken):
+                    next_state = i + 1
+                for j in range(i, 0):
+                    if self.stateToken[j] == ".*":
+                        pre_common_state = j
+                        break
+                self.states.append(self.getSingleAcceptedFunction\
+                    (self.stateToken[i], next_state, pre_common_state))
+            elif len(self.stateToken[i]) == 2:
+                if i + 1 < len(self.stateToken):
+                    next_state = i + 1
+                    next_expect_value = self.stateToken[i + 1]
+                for j in range(i, 0):
+                    if self.stateToken[j] == ".*":
+                        pre_common_state = j
+                        break
+                self.states.append(self.getRepetAcceptedFunction\
+                    (self.stateToken[i], next_expect_value, \
+                        i, next_state, pre_common_state))
+    def accept(self, str):
+        current_state = self.states[0]
+        print(current_state)
+        for i in str:
+            accepted = current_state(i)
+            print("-------", i)
+            print(accepted)
+            print("********")
+            if accepted:
+                print("accepted")
+                if accepted[0] and accepted[1] != None:
+                    current_state = accepted[1]
+                else:
+                    return True
+            else:
+                return False
+        if current_state == None:
+            return True
+        else:
+            return False
+
+test = StateController("a*b*b")
+teststr = "ab"
+test.genstates()
+print("accept", test.accept(teststr))
+
+
