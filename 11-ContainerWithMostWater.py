a9a6493e946d428d7e07d76aa6b4949c0d1109de
hongshan <hongshan.mail@gmail.com>
2017-02-23 17:30:25 +0800

    NO.11 max water accepted

--- 11-ContainerWithMostWater.py
+++ 11-ContainerWithMostWater.py
@@ -4,4 +4,18 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
-        
+        i = 0
+        j = len(height) - 1
+        max_water = 0
+        while i < j :
+            temp = (j - i) * (height[i] \
+                if height[i] < height[j] else height[j])
+            if temp > max_water :
+                max_water = temp
+            if height[i] < height[j]:
+                i += 1
+            else:
+                j -= 1
+        return max_water
+# test = Solution()
+# print(test.maxArea([1,2,9,7,9,9]))
